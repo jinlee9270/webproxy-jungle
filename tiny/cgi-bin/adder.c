e066936b3f945d25d0a0b0fe82e8588ab64355d9
jinlee9270 <93522658+jinlee9270@users.noreply.github.com>
2021-12-23 14:19:41 +0900

    Update adder.c

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -1,10 +1,18 @@
-/*
- * adder.c - a minimal CGI program that adds two numbers together
- */
-/* $begin adder */
-#include "csapp.h"
+<!-- 숙제 문제 11.10 -->
+<!DOCTYPE html>
+<html>
 
-int main(void) {
-  exit(0);
-}
-/* $end adder */
+<head>
+  <meta charset="utf-8" />
+  <title>Tiny Server</title>
+</head>
+
+<body>
+  <form action="/cgi-bin/adder" method="GET">
+    <p>first number: <input type="text" name="first" /></p>
+    <p>second number: <input type="text" name="second" /></p>
+    <input type="submit" value="Submit" />
+  </form>
+</body>
+
+</html>
