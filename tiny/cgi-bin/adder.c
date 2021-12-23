23f36b63d16a8b6c650dc464dffe4b0abdca186a
jinlee9270 <93522658+jinlee9270@users.noreply.github.com>
2021-12-23 14:20:12 +0900

    Update adder.c

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -1,18 +1,32 @@
-<!-- 숙제 문제 11.10 -->
-<!DOCTYPE html>
-<html>
+/*
+ * adder.c - a minimal CGI program that adds two numbers together
+ */
+/* $begin adder */
+#include "csapp.h"
 
-<head>
-  <meta charset="utf-8" />
-  <title>Tiny Server</title>
-</head>
+int main(void) {
+  char *buf, *p;
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE], arg3[MAXLINE], arg4[MAXLINE];
+  int n1=0, n2=0;
 
-<body>
-  <form action="/cgi-bin/adder" method="GET">
-    <p>first number: <input type="text" name="first" /></p>
-    <p>second number: <input type="text" name="second" /></p>
-    <input type="submit" value="Submit" />
-  </form>
-</body>
+  // Extract the two arguments
+  if ((buf = getenv("QUERY_STRING")) != NULL) {
+    p = strchr(buf, '&');
+    *p = '\0';  // it means string??
+    sscanf(buf, "first=%d", &n1);
+    sscanf(p+1, "second=%d", &n2);
+  }
+  sprintf(content, "QUERY_STRING=%s", buf);
+  sprintf(content, "Welcome to add.com: ");
+  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
+  sprintf(content, "%sThanks for visiting!\r\n", content);
 
-</html>
+  printf("Connection: close\r\n");
+  printf("Content-length: %d\r\n", (int)strlen(content));
+  printf("Content-type: text/html\r\n\r\n");
+  printf("%s", content);
+
+  fflush(stdout);
+  exit(0);
+}
+/* $end adder */
